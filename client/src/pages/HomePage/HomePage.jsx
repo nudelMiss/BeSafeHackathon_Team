1c735ea9d99b9345483cc4171b026b69caf3fdee
michal <michalnu@post.bgu.ac.il>
2025-12-27 17:59:50 +0200

    Deleted the changes in client

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,11 @@
-// import styles from './Home.module.css';
-// import RandomDuck from '../../components/RandomDuck/RandomDuck.jsx';
-
-
-// const Home = () => {
-//   return (
-//     <div className={styles.home}>
-//       <h1 className={styles.headline}>Duck It</h1>
-//       <RandomDuck />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-import styles from "./Home.module.css";
-import AnalyzeMessage from "../../components/AnalyzeMessage/AnalyzeMessage.jsx";
+import styles from './Home.module.css';
+import RandomDuck from '../../components/RandomDuck/RandomDuck.jsx';
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h1 className={styles.headline}>Analyze</h1>
-      <AnalyzeMessage />
+      <h1 className={styles.headline}>Duck It</h1>
+      <RandomDuck />
     </div>
   );
 };
